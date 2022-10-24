ac383b64d995980086eda1c94a97acb7f2d4b353
AngelR <larobledog@gmail.com>
2022-10-23 19:41:57 -0500

    Feat:add configuration to connection

---
+++ constants.js
@@ -0,0 +1,14 @@
+const DB_USER="admin";
+const DB_PASSWORD="admin";
+const DB_HOST="mern.8c1jlpr.mongodb.net";
+
+const API_VERSION="v1";
+const IP_SERVER="localhost";
+
+module.exports={
+DB_USER,
+DB_PASSWORD,
+DB_HOST,
+API_VERSION,
+IP_SERVER
+}
